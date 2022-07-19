ac5e3ab3c9856de8f6f5479be3e8ce745423f761
lzimp <dxlin@impcas.ac.cn>
2022-07-19 11:55:19 +0800

    data updated on July 19, 11:55

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 # Notice:
 # 1. Both cases are the same day bewteen 00-24 according to the source!
 # 2. Data sources: http://lz.bendibao.com/news/2022717/56801.shtm for data on July 16, 2022
-# 3. Data sources: http://gansu.gansudaily.com.cn/system/2022/07/18/030593524.shtml for data on July 17, 2022
+# 3. Data sources: http://gansu.gansudaily.com.cn/system/2022/07/18/030593524.shtml for data on July 17, 2022i
+# 4. Data sources: http://wsjk.gansu.gov.cn/wsjk/c112713/202207/2087231.shtml for data on July 18, 2022
--- pymacro_lzCovid19.py
+++ pymacro_lzCovid19.py
@@ -6,7 +6,7 @@
 # Date: July 16, 2022
 #==============================================================
 
-import os, sys, platform, math
+import os, sys, platform, math, time
 import datetime as dt
 import pickle as pckl
 import numpy as np
@@ -18,26 +18,29 @@ import matplotlib.pyplot as plt
 
 def datCovid():
     cvDat = pd.read_excel("lanzhou_covid-19_202207.xlsx", engine='openpyxl')
-    cvDat.rename(columns = {'Unnamed: 0':'date', 'Unnamed: 1':'week-day', 'Unnamed: 3':'Lanzhou1', 'Unnamed: 5':'Chengguan1'}, inplace=True)
+    #cvDat.rename(columns = {'Unnamed: 0':'date', 'Unnamed: 1':'week-day', 'Unnamed: 3':'Lanzhou1', 'Unnamed: 5':'Chengguan1'}, inplace=True)
     #print(cvDat['date'][idt].date(), cvDat['Lanzhou'][idt])
     #print(cvDat)
-    for idt in range(1, 28):
-        cvDat['date'][idt] = cvDat['date'][idt].date()
+    #cvDat = pd.read_csv('lanzhou_covid19_2207.dat', names=['date', 'lzcon', 'lzasy', 'cgcon', 'cgasy', 'cc1st', 'cc2nd'], header=1)
+    print(cvDat)
+    ndate = cvDat.shape[0]
+    #for idt in range(ndate):
+    #    cvDat['date'][idt] = cvDat['date'][idt].date()
 
-    start, end = 3, 14
+    #start, end = 3, 14
     fig, axs = plt.subplots(1, 1, constrained_layout=True)
-    axs.plot(cvDat['date'][start:end], cvDat['Lanzhou'][start:end], '-or', label="Lanzhou Confirmed")
-    axs.plot(cvDat['date'][start:end], cvDat['Lanzhou1'][start:end], '-.dr', label="Lanzhou Asympomatic")
-    axs.plot(cvDat['date'][start:end], cvDat['Chengguan'][start:end], '-ob', label="Chengguan Confirmed")
-    axs.plot(cvDat['date'][start:end], cvDat['Chengguan1'][start:end], '-.db', label="Chengguan Asympomatic")
+    axs.plot(cvDat['date'], cvDat['lzcon'], '-or', label="Lanzhou Confirmed")
+    axs.plot(cvDat['date'], cvDat['lzasy'], '-.dr', label="Lanzhou Asympomatic")
+    axs.plot(cvDat['date'], cvDat['cgcon'], '-ob', label="Chengguan Confirmed")
+    axs.plot(cvDat['date'], cvDat['cgasy'], '-.db', label="Chengguan Asympomatic")
     axs.tick_params(axis='x', labelrotation=45)
     axs.set_xlabel("Date", fontsize=16, horizontalalignment='right', x=1.0)
     axs.set_ylabel("Number of Cases", fontsize=16, horizontalalignment='right', y=1.0)
 
     ax2 = axs.twinx()
     ax2.set_ylabel("Number of Close Contact", color='c', fontsize=16, horizontalalignment='right', y=1.0)
-    ax2.plot(cvDat['date'][start:end+1], cvDat['cc_1st'][start:end+1], '--pc', label="1st close contact")
-    ax2.plot(cvDat['date'][start:end+1], cvDat['cc_2nd'][start:end+1], '--sc', label="2nd close contact")
+    ax2.plot(cvDat['date'], cvDat['cc1st'], '--pc', label="1st close contact")
+    ax2.plot(cvDat['date'], cvDat['cc2nd'], '--sc', label="2nd close contact")
 
 
     axs.legend(loc='best', facecolor='whitesmoke', edgecolor='black', fontsize=10)
@@ -45,9 +48,14 @@ def datCovid():
     plt.grid(axis='x', which='major', linestyle='--')
     plt.grid(axis='y', which='major', linestyle='--')
     
-    #plt.show()
-    plt.savefig("lanzhou_covid19_2207.png", dpi=200)
-    
+    plt.show()
+    #plt.savefig("lanzhou_covid19_2207.png", dpi=200)
+    #plt.clear()
+
+    #cvDat['lzConAve'][start:end] = cvDat['Lanzhou'][start:end].rolling(7).mean()
+    #print(cvDat)
+
+
 
 def main():
 
