80cdc3cd1898b914727db6df170527390add2fac
lzimp <dxlin@impcas.ac.cn>
2022-07-20 11:50:58 +0800

    data updated on July 19, 11:50

--- pymacro_lzCovid19.py
+++ pymacro_lzCovid19.py
@@ -16,12 +16,23 @@ import matplotlib.pyplot as plt
 #plt.style.use('classic')
 #print(mpl.matplotlib_fname())
 
-def datCovid():
-    cvDat = pd.read_excel("lanzhou_covid-19_202207.xlsx", engine='openpyxl')
-    #cvDat.rename(columns = {'Unnamed: 0':'date', 'Unnamed: 1':'week-day', 'Unnamed: 3':'Lanzhou1', 'Unnamed: 5':'Chengguan1'}, inplace=True)
-    #print(cvDat['date'][idt].date(), cvDat['Lanzhou'][idt])
-    #print(cvDat)
-    #cvDat = pd.read_csv('lanzhou_covid19_2207.dat', names=['date', 'lzcon', 'lzasy', 'cgcon', 'cgasy', 'cc1st', 'cc2nd'], header=1)
+
+class lzCovidData(object):
+    def __init__(self, dtfile):
+        self.file = dtfile
+
+    def loadData(self):
+
+        self.lzCovidData = pd.read_excel(self.file, engine='openpyxl')
+
+
+
+def datCovid(lzfile):
+
+    lzCovid = lzCovidData(lzfile)
+    lzCovid.loadData()
+    cvDat = lzCovid.lzCovidData
+
     print(cvDat)
     ndate = cvDat.shape[0]
     #for idt in range(ndate):
@@ -29,6 +40,10 @@ def datCovid():
 
     #start, end = 3, 14
     fig, axs = plt.subplots(1, 1, constrained_layout=True)
+    tday = str(dt.date.today())
+    axs.text(0.75, 0.95, 'by @lzimp (%s)'%(tday), transform=axs.transAxes, fontsize=8, color='gray', 
+            alpha=0.25, ha='center', va='center', rotation='0')
+
     axs.plot(cvDat['date'], cvDat['lzcon'], '-or', label="Lanzhou Confirmed")
     axs.plot(cvDat['date'], cvDat['lzasy'], '-.dr', label="Lanzhou Asympomatic")
     axs.plot(cvDat['date'], cvDat['cgcon'], '-ob', label="Chengguan Confirmed")
@@ -49,17 +64,55 @@ def datCovid():
     plt.grid(axis='y', which='major', linestyle='--')
     
     #plt.show()
-    plt.savefig("lanzhou_covid19_2207.png", dpi=200)
+    plt.savefig("lanzhou_cvdCases2207.png", dpi=200)
     #plt.clear()
 
     #cvDat['lzConAve'][start:end] = cvDat['Lanzhou'][start:end].rolling(7).mean()
     #print(cvDat)
 
+def lzDataStats(lzfile):
+
+    lzCovid = lzCovidData(lzfile)
+    lzCovid.loadData()
+    cvDat = lzCovid.lzCovidData
+    tsdate = cvDat['date']
+
+    cvDat['lzpos'] = cvDat['lzcon'] + cvDat['lzasy'] - cvDat['lzasytocon']
+    lzpstv = cvDat['lzpos']
+    cvDat['cgpos'] = cvDat['cgcon'] + cvDat['cgasy'] - cvDat['cgasytocon']
+    cgpstv = cvDat['cgpos']
+    lzrTot = cvDat['lzpos'].cumsum()
+    cgrTot = cvDat['cgpos'].cumsum()
+
+    lzravg = cvDat['lzpos'].rolling(window=7).mean()
+    cgravg = cvDat['cgpos'].rolling(window=7).mean()
+    #print(lzravg, cgravg)
+
+    fig, axs = plt.subplots(1, 1, constrained_layout=True)
+    tday = str(dt.date.today())
+    axs.text(0.75, 0.95, 'by @lzimp (%s)'%(tday), transform=axs.transAxes, fontsize=8, color='gray', 
+            alpha=0.25, ha='center', va='center', rotation='0')
+
+    axs.bar(tsdate, lzpstv, label='LZ new pos')
+    axs.plot(tsdate, lzravg, '-or', label='LZ 7ds avg')
+
+    axs.tick_params(axis='x', labelrotation=45)
+    axs.set_xlabel("Date", fontsize=16, horizontalalignment='right', x=1.0)
+    axs.set_ylabel("Number of Cases", fontsize=16, horizontalalignment='right', y=1.0)
+
+    axs.legend(loc='best', facecolor='whitesmoke', edgecolor='black', fontsize=10)
+    plt.grid(axis='x', which='major', linestyle='--')
+    plt.grid(axis='y', which='major', linestyle='--')
+
+    #plt.show()     
+    plt.savefig("lanzhou_pstvStats2207.png", dpi=200)
 
 
 def main():
 
-    datCovid()
+    lzfile = "/home/dxlin/jobs/csLearn/lzCovid19/lanzhou_covid-19_202207.xlsx"
+    datCovid(lzfile)
+    lzDataStats(lzfile)
 
 if __name__ == '__main__':
 
