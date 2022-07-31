552a5315bb455d35943b6c2df3fb89e631d72b6a
lzimp <dxlin@impcas.ac.cn>
2022-07-31 21:54:15 +0800

    data updated on July 31

--- README.md
+++ README.md
@@ -10,3 +10,16 @@
 # 7. Data sources: http://wsjk.gansu.gov.cn/wsjk/c112713/202207/2088510.shtml; for data on July 20, 2022
 # 8. Data sources: http://lz.bendibao.com/news/2022722/56888.shtm; for data on July 21, 2022
 # 9. Data sources: http://wsjk.gansu.gov.cn/wsjk/c112713/202207/2089869.shtml; for data on July 22, 2022
+#10. Data sources: http://wsjk.gansu.gov.cn/wsjk/c112713/202207/2090197.shtml; for data on July 23, 2022
+#11. Data sources: http://lz.bendibao.com/news/2022725/56917.shtm; for data on July 24, 2022
+#12. Data sources: http://wsjk.gansu.gov.cn/wsjk/c112713/202207/2091417.shtml; for data on July 25, 2022
+#12. Data sources: http://wsjk.gansu.gov.cn/wsjk/c112713/202207/2091417.shtml; for data on July 26, 2022
+#14. Data sources: http://wsjk.gansu.gov.cn/wsjk/c112713/202207/2092618.shtml; for data on July 27, 2022
+#15. Data sources: http://wsjk.gansu.gov.cn/wsjk/c112713/202207/2093221.shtml; for data on July 28, 2022
+#16. Data sources: http://wsjk.gansu.gov.cn/wsjk/c112713/202207/2093704.shtml; for data on July 29, 2022
+#17. Data sources: http://wsjk.gansu.gov.cn/wsjk/c112713/202207/2093994.shtml; for data on July 30, 2022
+
+
+
+# the following information is for Chengdu pandemic
+# 1. Data sources: http://cd.bendibao.com/news/2020128/118443.shtm; for the '7.20' pandemic data recorded from July 22 to 29, 2022
--- pymacro_lzCovid19.py
+++ pymacro_lzCovid19.py
@@ -15,23 +15,24 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 #plt.style.use('classic')
 #print(mpl.matplotlib_fname())
+from matplotlib.font_manager import FontProperties
+font = FontProperties(fname='/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc', size=10)
 
-
-class lzCovidData(object):
+class covidData(object):
     def __init__(self, dtfile):
         self.file = dtfile
 
     def loadData(self):
 
-        self.lzCovidData = pd.read_excel(self.file, engine='openpyxl')
+        self.covidData = pd.read_excel(self.file, engine='openpyxl')
 
 
 
 def datCovid(lzfile):
 
-    lzCovid = lzCovidData(lzfile)
+    lzCovid = covidData(lzfile)
     lzCovid.loadData()
-    cvDat = lzCovid.lzCovidData
+    cvDat = lzCovid.covidData
 
     print(cvDat)
     ndate = cvDat.shape[0]
@@ -72,9 +73,9 @@ def datCovid(lzfile):
 
 def lzDataStats(lzfile):
 
-    lzCovid = lzCovidData(lzfile)
+    lzCovid = covidData(lzfile)
     lzCovid.loadData()
-    cvDat = lzCovid.lzCovidData
+    cvDat = lzCovid.covidData
     tsdate = cvDat['date']
 
     cvDat['lzpos'] = cvDat['lzcon'] + cvDat['lzasy'] - cvDat['lzasytocon']
@@ -93,19 +94,68 @@ def lzDataStats(lzfile):
     axs.text(0.75, 0.95, 'by @lzimp (%s)'%(tday), transform=axs.transAxes, fontsize=8, color='gray', 
             alpha=0.25, ha='center', va='center', rotation='0')
 
-    axs.bar(tsdate, lzpstv, label='LZ new pos')
-    axs.plot(tsdate, lzravg, '-or', label='LZ 7ds avg')
+    #axs.bar(tsdate, lzpstv, label='每日新增阳性')
+    axs.bar(tsdate, lzpstv, label='LZ daily pos')
+    #axs.plot(tsdate, lzravg, '-or', label='七日平均阳性')
+    axs.plot(tsdate, lzravg, '-or', label='LZ 7days avg')
 
     axs.tick_params(axis='x', labelrotation=45)
     axs.set_xlabel("Date", fontsize=16, horizontalalignment='right', x=1.0)
     axs.set_ylabel("Number of Cases", fontsize=16, horizontalalignment='right', y=1.0)
 
-    axs.legend(loc='best', facecolor='whitesmoke', edgecolor='black', fontsize=10)
+    axs.legend(loc='upper left', facecolor='whitesmoke', edgecolor='black', fontsize=10)
     plt.grid(axis='x', which='major', linestyle='--')
     plt.grid(axis='y', which='major', linestyle='--')
 
     #plt.show()     
     plt.savefig("lanzhou_pstvStats2207.png", dpi=200)
+
+def cdDataStats(cdfile):
+
+    cdCovid = covidData(cdfile)
+    cdCovid.loadData()
+    cvDat = cdCovid.covidData
+    tsdate = cvDat['date']
+
+    cvDat['cdpos22'] = cvDat['cdcon22'] + cvDat['cdasy22'] - cvDat['cdasytocon22']
+    cdpstv22 = cvDat['cdpos22']
+    cdrTot22 = cvDat['cdpos22'].cumsum()
+    cdravg22 = cvDat['cdpos22'].rolling(window=7).mean()
+
+    cvDat['cdpos15'] = cvDat['cdcon15'] + cvDat['cdasy15'] - cvDat['cdasytocon15']
+    cdpstv15 = cvDat['cdpos15']
+    cdrTot15 = cvDat['cdpos15'].cumsum()
+    cdravg15 = cvDat['cdpos15'].rolling(window=7).mean()
+
+    cvDat['cdpos22'] = cvDat['cdpos22'].fillna(0)
+    cvDat['cdpos'] = cvDat['cdpos15'] + cvDat['cdpos22']
+    cdtwoavg = cvDat['cdpos'].rolling(window=7).mean()
+
+    #print(lzravg, cgravg)
+    print(cvDat)
+
+    fig, axs = plt.subplots(1, 1, constrained_layout=True)
+    tday = str(dt.date.today())
+    axs.text(0.75, 0.95, 'by @lzimp (%s)'%(tday), transform=axs.transAxes, fontsize=8, color='gray', 
+            alpha=0.25, ha='center', va='center', rotation='0')
+
+    axs.bar(tsdate, cdpstv22, alpha=0.75, label='CD daily pos (0722)')
+    axs.plot(tsdate, cdravg22, '-or', label='CD 7days avg (0722)')
+
+    axs.bar(tsdate, cdpstv15, color='g', alpha=0.75, label='CD daily pos (0715)')
+    axs.plot(tsdate, cdravg15, '-dm', label='CD 7days avg (0715)')
+    axs.plot(tsdate, cdtwoavg, '-p', color='orange', label='CD 7days avg (total)')
+
+    axs.tick_params(axis='x', labelrotation=45)
+    axs.set_xlabel("Date", fontsize=16, horizontalalignment='right', x=1.0)
+    axs.set_ylabel("Number of Cases", fontsize=16, horizontalalignment='right', y=1.0)
+
+    axs.legend(loc='upper left', facecolor='whitesmoke', edgecolor='black', fontsize=10)
+    plt.grid(axis='x', which='major', linestyle='--')
+    plt.grid(axis='y', which='major', linestyle='--')
+
+    #plt.show()     
+    plt.savefig("chengdu_pstvStats2207.png", dpi=200)
 
 
 def main():
@@ -113,6 +163,8 @@ def main():
     lzfile = "/home/dxlin/jobs/csLearn/lzCovid19/lanzhou_covid-19_202207.xlsx"
     datCovid(lzfile)
     lzDataStats(lzfile)
+    cdfile = "/home/dxlin/jobs/csLearn/lzCovid19/chengdu_covid19.xlsx"
+    cdDataStats(cdfile)
 
 if __name__ == '__main__':
 
